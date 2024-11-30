fddda6e93af84816f4f116446201257b51c77bd5
Anderson dos Santos Bauer <andersondossantosbauer@gmail.com>
2024-11-30 11:14:20 -0300

    correção de bugs

--- resources/views/pokemon/create.blade.php
+++ resources/views/pokemon/create.blade.php
@@ -28,13 +28,12 @@
                 <label for="coach_id" class="block mb-2 text-sm font-medium text-gray-900 dark:text-black text-left ">Trainer</label>
                 <select id="coach_id" name="coach_id" class="bg-gray-50 border border-gray-300 text-black text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500">
             </div>
-
-            <option selected value="">Choose a Trainer</option>
-            @foreach($coaches as $coach)
-            <option value="{{ $coach->id}}">{{ $coach->nome }}</option>
-            @endforeach
-            </select>
-            <button type="submit" class="  mt-5 text-dark bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-black-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Create Pokemon</button>
+                <option selected value="">Choose a Trainer</option>
+                @foreach($coaches as $coach)
+                <option value="{{ $coach->id}}">{{ $coach->nome }}</option>
+                @endforeach
+                </select>
+                <button type="submit" class="  mt-5 text-dark bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-black-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Create Pokemon</button>
             </div>
         </div>
     </form>
